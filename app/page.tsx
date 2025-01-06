89cf1707e6d0a592947e5555649a36518d8b19d1
aziznal <aziznal.dev@gmail.com>
2025-01-06 22:09:39 +0300

    add trails

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
-import { useScreenSize } from "@/hooks/useScreenSize";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef } from "react";
 
 export const dynamic = "force-dynamic";
 
@@ -18,61 +17,60 @@ const MATRIX_CHARS = [
     'ﾔ', 'ﾕ', 'ﾖ',
     'ﾗ', 'ﾘ', 'ﾙ', 'ﾚ', 'ﾛ',
     'ﾜ', 'ｦ', 'ﾝ',
-    // Hiragana
-    'あ', 'い', 'う', 'え', 'お',
-    'か', 'き', 'く', 'け', 'こ',
-    'さ', 'し', 'す', 'せ', 'そ',
-    'た', 'ち', 'つ', 'て', 'と',
-    'な', 'に', 'ぬ', 'ね', 'の',
-    'は', 'ひ', 'ふ', 'へ', 'ほ',
-    'ま', 'み', 'む', 'め', 'も',
-    'や', 'ゆ', 'よ',
-    'ら', 'り', 'る', 'れ', 'ろ',
-    'わ', 'を', 'ん',
-    // Latin Alphabet (Uppercase)
-    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
-    'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
-    'U', 'V', 'W', 'X', 'Y', 'Z',
-    // Greek Letters
+    // // Hiragana
+    // 'あ', 'い', 'う', 'え', 'お',
+    // 'か', 'き', 'く', 'け', 'こ',
+    // 'さ', 'し', 'す', 'せ', 'そ',
+    // 'た', 'ち', 'つ', 'て', 'と',
+    // 'な', 'に', 'ぬ', 'ね', 'の',
+    // 'は', 'ひ', 'ふ', 'へ', 'ほ',
+    // 'ま', 'み', 'む', 'め', 'も',
+    // 'や', 'ゆ', 'よ',
+    // 'ら', 'り', 'る', 'れ', 'ろ',
+    // 'わ', 'を', 'ん',
+    // // Latin Alphabet (Uppercase)
+    // 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+    // 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+    // 'U', 'V', 'W', 'X', 'Y', 'Z',
+    // // Greek Letters
     'Α', 'Β', 'Γ', 'Δ', 'Ε',
     'Ζ', 'Η', 'Θ', 'Ι', 'Κ',
     'Λ', 'Μ', 'Ν', 'Ξ', 'Ο',
     'Π', 'Ρ', 'Σ', 'Τ', 'Υ',
     'Φ', 'Χ', 'Ψ', 'Ω',
-    // Cyrillic Letters
-    'А', 'Б', 'В', 'Г', 'Д',
-    'Е', 'Ё', 'Ж', 'З', 'И',
-    'Й', 'К', 'Л', 'М', 'Н',
-    'О', 'П', 'Р', 'С', 'Т',
-    'У', 'Ф', 'Х', 'Ц', 'Ч',
-    'Ш', 'Щ', 'Ъ', 'Ы', 'Ь',
-    'Э', 'Ю', 'Я',
-    // Chinese Characters
-    '中', '国', '文', '字', '雨', '電', '話', '車', '山', '水',
-    // Numbers
-    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-    // Symbols
-    '!', '@', '//', '$', '%', '^', '&', '*', '(', ')',
-    '-', '_', '+', '=', '[', ']', '{', '}', '|', '\\',
-    ':', ';', '"', '\'', '<', '>', ',', '.', '?', '/',
-    '~', '`', '✓', '✔', '✕', '✖', '★', '☆', '○', '●',
-    // Mathematical Symbols
-    '∞', '≠', '≡', '≤', '≥', '±', '∑', '∏', '∫', '√',
-    '∆', '∇', '∂', '∈', '∉', '∅', '∧', '∨', '⊕', '⊗',
-    // Miscellaneous Symbols
-    '♠', '♣', '♥', '♦', '♤', '♧', '♡', '♢', '☺', '☻',
-    '♂', '♀', '♪', '♫', '☼', '§', '¤', '©', '®', '™'
+    // // Cyrillic Letters
+    // 'А', 'Б', 'В', 'Г', 'Д',
+    // 'Е', 'Ё', 'Ж', 'З', 'И',
+    // 'Й', 'К', 'Л', 'М', 'Н',
+    // 'О', 'П', 'Р', 'С', 'Т',
+    // 'У', 'Ф', 'Х', 'Ц', 'Ч',
+    // 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь',
+    // 'Э', 'Ю', 'Я',
+    // // Chinese Characters
+    // '中', '国', '文', '字', '雨', '電', '話', '車', '山', '水',
+    // // Numbers
+    // '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+    // // Symbols
+    // '!', '@', '//', '$', '%', '^', '&', '*', '(', ')',
+    // '-', '_', '+', '=', '[', ']', '{', '}', '|', '\\',
+    // ':', ';', '"', '\'', '<', '>', ',', '.', '?', '/',
+    // '~', '`', '✓', '✔', '✕', '✖', '★', '☆', '○', '●',
+    // // Mathematical Symbols
+    // '∞', '≠', '≡', '≤', '≥', '±', '∑', '∏', '∫', '√',
+    // '∆', '∇', '∂', '∈', '∉', '∅', '∧', '∨', '⊕', '⊗',
+    // // Miscellaneous Symbols
+    // '♠', '♣', '♥', '♦', '♤', '♧', '♡', '♢', '☺', '☻',
+    // '♂', '♀', '♪', '♫', '☼', '§', '¤', '©', '®', '™'
 ]
 
-const FONT_SIZE = 24;
+/** Amount of characters trailing behind */
+const TRAIL_LENGTH = 5;
+
+/** Font size in pixels */
+const FONT_SIZE = 30;
 
 export default function Home() {
-  const { screenWidth, screenHeight } = useScreenSize();
-
   const canvasRef = useRef<HTMLCanvasElement>(null);
-
-  const rowCount = Math.ceil(screenHeight / (FONT_SIZE + 1));
-  const colCount = Math.ceil(screenWidth / FONT_SIZE);
 
   useEffect(() => {
     const canvas = canvasRef.current;
@@ -87,46 +85,107 @@ export default function Home() {
     canvas.height = window.innerHeight;
 
     const getNewPosY = (currentPosY: number, multiplier?: number) => {
-      if (currentPosY > canvas.height + 40) return -20;
+      const bottomMargin = FONT_SIZE * (TRAIL_LENGTH + 1); // +1 comes from the head char
+
+      if (currentPosY > canvas.height + bottomMargin) return -20;
       return currentPosY + 1 * (multiplier ?? 1);
     };
 
-    const drawChar = (x: number) => {
-      let y = getRandomInt(0, canvas.height);
+    class Char {
+      x: number;
+      y: number;
+      ySpeedMultiplier: number;
+      char: string;
+      trailChars: string[];
 
-      const ySpeedMultiplier = getRandomFloat(1, 3);
+      constructor(args: { x: number }) {
+        if (!ctx || !canvas) throw new Error("Canvas or Context is undefined.");
 
-      let char = getRandomChar(MATRIX_CHARS);
+        this.x = args.x;
 
-      setInterval(() => {
-        char = getRandomChar(MATRIX_CHARS);
-      }, 350 * ySpeedMultiplier);
+        this.y = getRandomInt(0, canvas?.height);
 
-      const draw = () => {
-        ctx.font = "30px JetBrainsMono Nerd Font";
+        this.ySpeedMultiplier = getRandomFloat(1, 1.5);
 
-        ctx.fillText(char, x, y);
+        this.char = getRandomChar(MATRIX_CHARS);
+        this.trailChars = Array(TRAIL_LENGTH)
+          .fill(0)
+          .map(() => getRandomChar(MATRIX_CHARS));
 
+        // update character every few hundred ms
+        setInterval(() => {
+          this.char = getRandomChar(MATRIX_CHARS);
+        }, 200 * this.ySpeedMultiplier);
+      }
+
+      draw() {
+        if (!ctx || !canvas) throw new Error("Canvas or Context is undefined.");
+
+        ctx.save();
+
+        ctx.font = `${FONT_SIZE}px JetBrainsMono Nerd Font`;
         ctx.fillStyle = "lightgreen";
-        ctx.shadowColor = "green";
-        ctx.shadowOffsetX = 3;
-        ctx.shadowOffsetY = 3;
-        ctx.shadowBlur = 12;
+        ctx.shadowColor = "lime";
+        ctx.shadowOffsetX = 0;
+        ctx.shadowOffsetY = 0;
+        ctx.shadowBlur = 8;
 
-        y = getNewPosY(y, ySpeedMultiplier);
-      };
+        ctx.translate(canvas.width, 0);
+        ctx.scale(-1, 1);
+        ctx.fillText(this.char, this.x, this.y);
 
-      return {
-        draw,
-      };
-    };
+        this.y = getNewPosY(this.y, this.ySpeedMultiplier);
 
-    const chars = Array(45)
+        ctx.restore();
+
+        this.drawTrail();
+      }
+
+      drawTrail() {
+        if (!ctx || !canvas) throw new Error("Canvas or Context is undefined.");
+
+        this.trailChars.forEach((char, i) => {
+          ctx.save();
+
+          ctx.font = `${FONT_SIZE}px JetBrainsMono Nerd Font`;
+
+          const opacity =
+            (100 * (this.trailChars.length - (i + 1))) / this.trailChars.length;
+
+          ctx.fillStyle = `rgb(144 238 144 / ${opacity}%)`;
+
+          ctx.shadowColor = "lime";
+          ctx.shadowOffsetX = 0;
+          ctx.shadowOffsetY = 0;
+          ctx.shadowBlur = 8;
+          ctx.fillText(char, this.x, this.y - 30 * (i + 1));
+
+          ctx.translate(canvas.width, 0);
+          ctx.scale(-1, 1);
+
+          ctx.restore();
+        });
+      }
+    }
+
+    const charWidth = 15;
+
+    const maxChars = Math.floor(canvas.width / charWidth);
+
+    const chars = Array(maxChars)
       .fill(0)
-      .map((_, i) => drawChar(20 * (i + 1)));
+      .map(
+        (_, i) =>
+          new Char({
+            x: charWidth * (i + 1),
+          }),
+      );
 
     const draw = () => {
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      ctx.save();
+      ctx.fillStyle = "rgb(5 16 1 / 75%)";
+      ctx.fillRect(0, 0, canvas.width, canvas.height);
+      ctx.restore();
 
       chars.forEach((c) => c.draw());
     };
@@ -143,27 +202,6 @@ export default function Home() {
       </canvas>
     </div>
   );
-
-  return (
-    <div
-      className="flex leading-none font-mono overflow-x-clip"
-      style={{
-        fontSize: FONT_SIZE,
-      }}
-    >
-      {Array(colCount)
-        .fill(0)
-        .map((_, i) => (
-          <div key={`col-${i}`} className="flex flex-col">
-            {Array(rowCount)
-              .fill(0)
-              .map((_, j) => (
-                <Char key={`col-${i}row-${j}`} index={j} />
-              ))}
-          </div>
-        ))}
-    </div>
-  );
 }
 
 function getRandomChar(chars: string[]) {
@@ -178,32 +216,3 @@ function getRandomInt(start: number, end: number): number {
 function getRandomFloat(start: number, end: number): number {
   return Math.random() * (end - start) + start;
 }
-
-const Char: React.FC<{ index: number }> = ({}) => {
-  const [char, setChar] = useState(getRandomChar(MATRIX_CHARS));
-
-  useEffect(() => {
-    const interval = setInterval(
-      () => {
-        setChar(getRandomChar(MATRIX_CHARS));
-      },
-      getRandomInt(200, 500),
-    );
-
-    return () => clearInterval(interval);
-  }, []);
-
-  return (
-    <span
-      className="antialiased text-green-400"
-      style={{
-        width: `${FONT_SIZE}px`,
-        height: `${FONT_SIZE}px`,
-        opacity: getRandomFloat(0, 1),
-        textShadow: "0 0 5px #fff",
-      }}
-    >
-      {char}
-    </span>
-  );
-};
